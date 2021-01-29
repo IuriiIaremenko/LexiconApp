57bfbf16f66b5bebd83ca986287218561dc5b3fc
Iurii Iaremenko <iurii.iaremenko@gmail.com>
2021-01-29 18:41:37 +0200

    Coming soon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Lexicon app coming soon...</h1>
       </header>
     </div>
   );
